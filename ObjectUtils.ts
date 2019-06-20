22de9c2869a6799aa1a9677a9382567740ac7c10
Nika AI <49638397+nika-ai@users.noreply.github.com>
2019-06-20 17:33:34 +0300

    Update ObjectUtils.ts

--- ObjectUtils.ts
+++ ObjectUtils.ts
@@ -1,4 +1,10 @@
+import objectAssignDeep from './ObjectAssignDeep';
+
 export class ObjectUtils {
+    static assignDeep<T>(...args:Array<{}>):T {
+        return objectAssignDeep.apply(objectAssignDeep, args);
+    }
+    
     static freezeDeep<T extends Object>(obj:{[key:string]:any}&T):Readonly<T> {
         const propNames = Object.getOwnPropertyNames(obj);
 
